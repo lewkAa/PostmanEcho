3c881714dc834c65e8eb5b589e35fc4a50b1f645
lewkAa <tihonovtan@gmail.com>
2025-04-06 16:29:40 +0300

    Intial commit

---
+++ src/test/java/ru/netology/test/EchoPost.java
@@ -0,0 +1,28 @@
+package ru.netology.test;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+class EchoPost {
+
+    @Test
+    void EchoPostmanTest() {
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("Alex")
+                .queryParam("active", true)
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("Mary"))
+                .body("args.active", equalTo("true"))
+                .body("url", equalTo("https://postman-echo.com/post?active=true"));
+
+    }
+}
